6c51fe06334c13419525d820232bd7494190f9e8
CP <csenge.petak@uvm.edu>
2020-11-06 11:34:37 -0500

    fixed some bugs

--- main.py
+++ main.py
@@ -49,12 +49,12 @@ def generate_optimum(nenvs, neffect):
             for i in range(neffect):
                 t = y[int((100/neffect)*i)]
                 t = np.around(t,2)
-                e.append(t)                                
+                e.append(t)
             envs.append(np.asarray(e))
     print("created envs")
     return(envs)
-            
-    
+
+
 def fitness_function(grn_out,envs,state,args):
     #distance of GRN output to perfect expression in specific environmental state
     if np.sum(grn_out) != 0:
@@ -76,7 +76,7 @@ def create_pop(args):
         pop.append(t)
 
     print("created pop")
-    return(pop) 
+    return(pop)
 
 class Agent:
     def __init__(self,ID,args):
@@ -87,120 +87,120 @@ class Agent:
         Returns:
             None. Constructor method.
         """
-        
+
         self.args = args #so that we can run experiments with various parameters
-        
+
         #BUILDING INITIAL GRN
         N = self.args.E +1
         self.N = N #number of initial nodes, maternal + effectors
         self.nodes = list(np.arange(N))
         self.adjM = np.zeros((N, N))
-        self.adjM[0,1:] = 0.1 
-        #initiate with maternal connected to effector genes 
+        self.adjM[0,1:] = 0.1
+        #initiate with maternal connected to effector genes
         self.adjM[0,0] = 1
         #and maternal positive feedbackloop (otherwise it turns itself down immediately and so the rest is also repressed)
-        
+
         #OTHER PARAMETERS
         self.ID = ID
         self.tree = [] #ID and tree for building phylogenic trees
         self.alpha = 10 #for sigmoid function - could be mutated later, now constant
         self.blacklist = [] #set of deleted genes
         self.fitness = 0
-        
+
         self.dn_rate = self.args.dn_rate
         self.dup_rate = self.args.dup_rate
         self.del_rate = self.args.del_rate
         self.edge_rate = self.args.edge_rate
         #these rates can later be under selection too
-        
-    
+
+
     def mutate(self):
-        
+
         if random.uniform(0, 1) < self.dn_rate:
             self.add_dn_node()
-            
+
         if random.uniform(0, 1) < self.dup_rate:
             self.add_dup_node()
-            
+
         if random.uniform(0, 1) < self.del_rate:
             self.del_node()
-            
+
         if random.uniform(0, 1) < self.edge_rate:
             self.mut_edge()
-    
-    def add_dn_node(self): 
+
+    def add_dn_node(self):
         #de novo gene, no interactions when initiated
         self.nodes.append(self.N)
 
         #Make adj matrix bigger
         column_to_be_added = np.zeros(self.N)
-        self.adjM = np.column_stack((self.adjM, column_to_be_added)) 
+        self.adjM = np.column_stack((self.adjM, column_to_be_added))
         self.N += 1
-        
+
         if self.N > 50:
             print("Number of nodes greater than 50!")
-        
+
         row_to_be_added = np.zeros(self.N)
         self.adjM = np.vstack((self.adjM, row_to_be_added))
-        
+
     def add_dup_node(self):
         #duplicate a node with all of its connections
         avail = list(set(self.nodes) - set(self.blacklist) - set(self.args.default_nodes))
-        
+
         if len(avail) > 0:
             dup_node = np.random.choice(avail)
 
             column_to_be_added = self.adjM[:,dup_node] #duplicate column
-            self.adjM = np.column_stack((self.adjM, column_to_be_added)) 
+            self.adjM = np.column_stack((self.adjM, column_to_be_added))
 
             row_to_be_added = self.adjM[dup_node,:] #duplicate row
             self.adjM = np.vstack((self.adjM, row_to_be_added))
 
             self.nodes.append(self.N)
             self.N += 1
-            
+
             if self.N > 50:
                 print("Number of nodes greater than 50!")
-        
+
     def del_node(self):
         #delete an existing node with is not an effector or maternal gene
         #rows/columns in adj matrix are not deleted, all interactions are set to 0 instead
         #self.N incudes deleted nodes!
         avail = list(set(self.nodes) - set(self.blacklist) - set(self.args.default_nodes))
-        
+
         if len(avail) > 0:
-        
+
             deleted_node = np.random.choice(avail)
 
             self.adjM[deleted_node,:] = np.zeros(self.N)
             self.adjM[:,deleted_node] = np.zeros(self.N)
 
             self.blacklist.append(deleted_node)
-        
+
     def mut_edge(self):
         #mutate the weight of an edge
         avail = list(set(self.nodes) - set(self.blacklist))
-        
+
         mod_avail_from = list(set(avail) - set(self.args.effectors)) #effectors can't regulate
         from_nodes = np.random.choice(mod_avail_from)
-        
+
         avail.remove(0) #maternal can't be regulated
-        
+
         if from_nodes != 0: #otherwise I would remove 0 twice
             avail.remove(from_nodes) #self loops are not allowed
-        
+
         to_nodes = np.random.choice(avail)
-        
+
         self.adjM[from_nodes,to_nodes] = np.around(np.random.normal(0, 1, 1), 2)
-        
-    
+
+
     def sigm(self,sum_input):
     #adapted from wang 2014
         sum_input = sum_input-0.5
         x = sum_input * -self.alpha
         output = 1/(1 + np.exp(x))
         return output
-    
+
     def grn_output(self, debug=False):
         a = self.adjM
         if debug:
@@ -212,7 +212,7 @@ class Agent:
         e=0 #counter for state stability
         i=0 #counter for number of GRN updates
         ss=[] #stores GRN state for comparision to updated state
-  
+
         while e < 2 and i < 3*(self.N-len(self.blacklist)):
             if debug:
                 print(s) #print expression state
@@ -241,14 +241,14 @@ def evolve(args):
     pop = create_pop(args)
     state = 0 # we start with environment 0
     envs = generate_optimum(args.nenv,args.E)
-    
+
     average_fitness = [] #for tracking average fitness in pop per generation
     ave_grnsize = [] #average GRN size
-    
+
     for i in trange(args.gens):
         #every generation
         #check fitness and generate based on that new population
-        
+
         grn_size=[]
 
         for idx, ind in enumerate(pop): #mutate and change ID
@@ -259,33 +259,33 @@ def evolve(args):
             grn_size.append(ind.N-len(ind.blacklist))
 
         ave_grnsize.append(np.average(grn_size))
-        
+
         for p in pop:
             grn_out = p.grn_output()
             p.fitness = fitness_function(grn_out,envs,state,args) #will depend on env
-                 
-        pop_fitness = [x.fitness for x in pop]  
+
+        pop_fitness = [x.fitness for x in pop]
         tot_fitness = np.sum(pop_fitness)
         average_fitness.append(np.mean(pop_fitness))
-        
+
 
         pop_prob = [f/tot_fitness for f in pop_fitness]
         new_pop = np.random.choice(pop, args.pop_size, p=pop_prob, replace = True) #same individual can be selected over and over again
         pop = [deepcopy(agent) for agent in new_pop]
-        
-        
+
+
         rd = (i+1) % args.season_length
-        
+
         if rd == 0:
             #print("after this changed state")
             if state == 0:
                 state = 1 #np.random.choice(nenvs)
             else:
                 state = 0
-                
+
     best_agent = max([{"fitness" : x.fitness, "Agent" : x} for x in pop], key=lambda x : x["fitness"])["Agent"]
 
-    return(pop)
+    return(pop, average_fitness)
 
 
 
@@ -316,7 +316,7 @@ if __name__ == "__main__":
     parser.add_argument('-rep', type=str, default="1", help="ID of replicate")
 
     args = parser.parse_args()
-  
+
     tag = conf2tag(vars(args))
 
     ### Important variables to set
@@ -326,9 +326,8 @@ if __name__ == "__main__":
     args.effectors = np.delete(args.default_nodes, 0) #effector genes don't have outgoing edges, only incoming
     args.name_of_file = args.exp_type + tag + "_" + args.rep
     print(args.name_of_file)
-    
 
 
     ### RUN CODE
     print("running code")
-    population, average_fitness = evolve(args.pop_size, args.gens, args.season_length, args)
+    population, average_fitness = evolve(args)
